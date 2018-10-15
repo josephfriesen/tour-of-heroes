87d2df2ac3ab05d0d507a192599e42d61bd4f948
Evan Filkins and Joe Friesen <evan@>
2018-10-15 16:41:28 -0700

    Work through angular tutorial through step 7- routing.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Stupid' },
+  { id: 12, name: 'Toilet Titan' },
+  { id: 13, name: 'Cool-thritis' },
+  { id: 14, name: 'Butt-zilla' },
+  { id: 15, name: 'Too Old For This Shit Man' },
+  { id: 16, name: 'Professor Eczema' },
+  { id: 17, name: 'Superman 2' },
+  { id: 18, name: 'Running Out Of Ideas Girl' }
+];
